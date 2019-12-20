23786a058dbfc08e877a29e282dcb99ae751da08
Saritha Pothuraju <saritha.pothuraju@appian.com>
2019-12-20 16:10:45 -0500

    Success

---
+++ ddl_files/store DDL.sql
@@ -0,0 +1,21 @@
+/*
+Data Store: store
+Created: 2019-12-20T20:59:09.230Z
+By: saritha.pothuraju.s
+Appian Version: 20.1.0.200
+Target Database: MySQL 5.7.26-log
+Database Driver: MySQL Connector Java mysql-connector-java-5.1.47 ( Revision: fe1903b1ecb4a96a917f7ed3190d80c049b1de29 )
+*/
+
+/* UPDATE DDL */
+/* DROP AND CREATE DDL */
+/* WARNING: The DDL commented out below will drop and re-create all tables.
+    drop table if exists `type`;
+
+    create table `type` (
+        `id` integer not null auto_increment,
+        `name` varchar(255),
+        `firstname` varchar(255),
+        primary key (`id`)
+    ) ENGINE=InnoDB;
+*/
